cd2afa64b324664abf651aaca943ff595f04eac6
murigi-johnson <johnsonmurigi7@gmail.com>
2022-09-02 15:24:22 +0300

    initial commit

---
+++ vba-project-sem2/LoginSystem/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox3.Text = "" Or TextBox4.Text = "" Then
+            MsgBox("Please Input some values")
+        Else
+            My.Settings.username = TextBox3.Text
+            My.Settings.password = TextBox4.Text
+            My.Settings.Save()
+            MsgBox("Successfully Registered")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = My.Settings.username AndAlso TextBox2.Text = My.Settings.password Then
+            MsgBox("Access Granted")
+        Else
+            MsgBox("Access Denied")
+        End If
+    End Sub
+End Class
---
+++ vba-project-sem2/LoginSystem/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LoginSystem")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("LoginSystem")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c69e3388-6c98-4a5a-852d-0256f6c24e81")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
